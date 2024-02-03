f023c75b74d84d8f33b6f18ce53222d1f3728576
Rinkal Shah <rnkl.shah@gmail.com>
2024-02-03 02:16:01 -0500

    Time: 364 ms (72.75%), Space: 17.7 MB (81.79%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,33 +1,25 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
+        """
         dp = [0]*len(nums)
         dp[0] = nums[0]
+        """
+        
+        maxJump = nums[0]
+        prev = maxJump
         if(len(nums) == 1):
             return True
         
         for i in range(len(nums)):
           
-            dp[i] = max(dp[i - 1], nums[i] + i)
-            if i > dp[i - 1]:
+            maxJump, prev = max(maxJump, nums[i] + i), maxJump
+            if i > prev:
                 return False
         
          
         return True
         
-        """
-        dp = [0 for i in range(len(nums))]
-        dp[0] = nums[0]
-        for i in range(len(nums)):
-            #print(nums[i])
-            #print(nums[i]+i, dp[i-1])
-            dp[i] = max(nums[i]+i, dp[i-1])
-            #print(dp)
-        
-            if i > dp[i-1]:
-                #print("cant reach")
-                return False
-        return True
-        """
+      
             
             
             
