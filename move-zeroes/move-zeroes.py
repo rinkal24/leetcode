1049cbf8a74d53fc47fef45fd8d61701b6077400
Rinkal Shah <rnkl.shah@gmail.com>
2024-02-08 00:34:22 -0500

    Time: 122 ms, Memory: 18.1 MB - LeetHub

--- move-zeroes/move-zeroes.py
+++ move-zeroes/move-zeroes.py
@@ -12,13 +12,11 @@ class Solution:
             ptr += 1
         ptr = 0
         
-        print(zeroptr, ptr)
         while(ptr < len(nums) and zeroptr >= 0):
             if nums[ptr] == 0 or ptr < zeroptr:
                 ptr += 1
                 continue
             else:
-                print(zeroptr, ptr)
                 nums[zeroptr] = nums[ptr]
                 nums[ptr] = 0
                 zeroptr += 1
