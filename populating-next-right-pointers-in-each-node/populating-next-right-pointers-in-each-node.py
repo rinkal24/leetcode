902f9a297135043c1b998e4e53a5b566a9916ada
Rinkal Shah <rnkl.shah@gmail.com>
2024-02-13 18:22:26 -0500

    Time: 50 ms, Memory: 17.9 MB - LeetHub

--- populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
+++ populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -7,24 +7,25 @@ class Node:
         self.right = right
         self.next = next
 """
-import collections
+
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
         if not root:
             return root
         
-        Q = collections.deque([root])
-        while Q:
-            size = len(Q) 
+        leftmost = root
+        while leftmost.left:
+            head = leftmost
             
-            for i in range(size):
-                node = Q.popleft()
-                if i < size - 1:
-                    node.next = Q[0]
-                if node.left:
-                    Q.append(node.left)
-                if node.right:
-                    Q.append(node.right)
+            while head:
+                head.left.next = head.right
+                if head.next:
+                    head.right.next = head.next.left
+                    
+                head = head.next
+            
+            
+            leftmost = leftmost.left
         return root
             
         
