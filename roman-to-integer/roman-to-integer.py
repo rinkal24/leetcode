c1fe9b8d31fb5038072c1e56f14cba942129a9c5
Rinkal Shah <rnkl.shah@gmail.com>
2024-02-14 19:33:06 -0500

    Time: 57 ms, Memory: 16.5 MB - LeetHub

---
+++ roman-to-integer/roman-to-integer.py
@@ -0,0 +1,13 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        d = {"X":10, "V":5, "I":1, "L":50, "C":100 , "D": 500, "M":1000}
+        total = 0
+        prev = s[0]
+        for i in s:
+            if (i in ["V", "X"] and prev == "I") or (i in ["L", "C"] and prev == "X") or (i in ["D", "M"] and prev == "C"):
+                total += d[i] - 2* d[prev]
+            else:
+                total += d[i]
+            prev = i
+            
+        return total
