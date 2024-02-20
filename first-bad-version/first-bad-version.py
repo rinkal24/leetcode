d7249d6823fef4720bc70fb5ce234b9c09b4547f
Rinkal Shah <rnkl.shah@gmail.com>
2024-02-19 19:19:46 -0500

    Time: 32 ms, Memory: 16.5 MB - LeetHub

--- first-bad-version/first-bad-version.py
+++ first-bad-version/first-bad-version.py
@@ -4,15 +4,13 @@
 class Solution:
     def firstBadVersion(self, n: int) -> int:
         lo, hi = 0, n 
-        minVal = hi
         
         while lo < hi:
             mid = int((lo + hi )/2)
             if isBadVersion(mid):
-                minVal = min(minVal, mid)
                 hi = mid
             else:
                 lo = mid + 1
-        return minVal
+        return lo
         
             
