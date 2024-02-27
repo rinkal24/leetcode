4c81f40f2a49b7f703c9f816eb23d0bef9fe49aa
Rinkal Shah <rnkl.shah@gmail.com>
2024-02-26 23:23:16 -0500

    Time: 8940 ms (5.00%), Space: 16.9 MB (40.02%) - LeetHub

--- 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.py
+++ 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.py
@@ -4,12 +4,11 @@ class Solution:
      
         maxLen = 0
         start = 0
-        ctr = collections.Counter() ###Counter(s[start:i + 1])
         
         for i in range(len(s)):
-            ctr[s[i]] += 1
+            ctr = Counter(s[start:i + 1])
             if len(ctr) <= k:
-                maxLen = max(maxLen, i - start + 1)
+                maxLen = max(maxLen, i - start)
             else:
                 ctr[s[start]] -= 1
                 if ctr[s[start]] == 0:
@@ -17,4 +16,4 @@ class Solution:
                 start += 1
             
             
-        return maxLen  if k > 0 else 0
+        return maxLen + 1 if k > 0 else 0
