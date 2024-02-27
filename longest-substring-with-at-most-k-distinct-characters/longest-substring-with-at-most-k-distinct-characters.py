4a8ba1a37d8067aeef55189ea287f2c43c5d45cb
Rinkal Shah <rnkl.shah@gmail.com>
2024-02-26 19:43:42 -0500

    Time: 9410 ms, Memory: 17 MB - LeetHub

---
+++ longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.py
@@ -0,0 +1,21 @@
+class Solution:
+    def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
+        
+     
+        maxLen = 0
+        start = 0
+        
+        for i in range(len(s)):
+            ctr = Counter(s[start:i + 1])
+            if len(ctr) <= k:
+                maxLen = max(maxLen, i - start)
+                ##print(s[:i])
+            else:
+                ctr[s[start]] -= 1
+                ##print(i, ctr, start, maxLen)
+                if ctr[s[start]] == 0:
+                    del ctr[s[start]]
+                start += 1
+            
+            
+        return maxLen + 1 if k > 0 else 0
