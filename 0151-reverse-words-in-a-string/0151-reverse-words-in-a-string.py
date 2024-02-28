cdbb4e4026372801dc8eeee795fad3ef61dfc3ad
Rinkal Shah <rnkl.shah@gmail.com>
2024-02-28 15:40:45 -0500

    Time: 38 ms (53.10%), Space: 16.6 MB (98.98%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,6 +1,5 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        ansList = []
         ans = ""
         res = ""
         for i in s:
@@ -8,21 +7,11 @@ class Solution:
                 ans += i
             else:
                 if len(ans) > 0:
-                    ansList.append(ans)
                     res = " "+ ans + res
                 ans = ""
                 continue
         
         if len(ans) > 0:
-            ansList.append(ans)
             res = ans + res + " "
-        total_words = len(ansList) - 1
-        print(res)
-        """
-        res = ""
         
-        while total_words >= 0:
-            res += ansList[total_words] + " "
-            total_words -= 1
-        """
         return res.strip()
