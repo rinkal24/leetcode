07a5a16c1b2f287c5d16c5a4b6be617047578505
Rinkal Shah <rnkl.shah@gmail.com>
2024-02-28 20:08:04 -0500

    Time: 56 ms (42.86%), Space: 16.8 MB (60.63%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,44 +1,34 @@
+import collections
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
         
-        d = {}
+        d = collections.defaultdict(list)
         nr = 0
         patternDown = True
         
         for i in range(len(s)):
-            if nr not in d:
-                d[nr] = [s[i]]
-            else:
-                d[nr] += s[i]
-                
-            if i + 1 == len(s):
-                break
+            d[nr].append(s[i])
             
-            elif nr == numRows - 1:
+            if nr == numRows - 1:
                 patternDown = False
                 if nr - 1 >= 0:
                     nr -= 1
-                continue
 
             elif nr == 0:
                 patternDown = True
                 nr = 1
-                continue
-
-            # (nr + 1 < numRows) and (nr > 0):
-            if patternDown:
-                nr += 1
+            
             else:
-                nr -= 1
+                if patternDown:
+                    nr += 1
+                else:
+                    nr -= 1
            
         res = ""
         i = 0
         
         while i < numRows and i < len(s):
             res += ''.join(d[i])
-            
             i += 1
             
-            
-       
         return res
