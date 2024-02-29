ab98a7108454e230bd090086e208bf26ccb368e3
Rinkal Shah <rnkl.shah@gmail.com>
2024-02-28 19:33:18 -0500

    Time: 479 ms (5.45%), Space: 23.7 MB (5.55%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -2,33 +2,32 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         d = [['' for _ in range(len(s))] for _ in range(numRows)]
         
-        
         nr = 0
         nc = 0
         patternDown = True
         
-        for i in s:
-            if nr == numRows and d[0][0] != 0:
-                patternDown = False
-                nr -= 2
-                nc += 1
-                
-            d[nr][nc] = i
+        for i in range(len(s)):
+            d[nr][nc] = s[i]
             
-            if (nr < numRows) and (nr >= 0):
-                if patternDown:
+            if i + 1 < len(s):
+                if nr == numRows - 1:
+                    patternDown = False
+                    nc += 1
+                    if nr - 1 >= 0:
+                        nr -= 1
+
+                elif nr == 0:
+                    patternDown = True
                     nr += 1
-                else:
-                    if nr == 0:
-                        patternDown = True
+
+                elif (nr + 1 < numRows) and (nr > 0):
+                    if patternDown:
                         nr += 1
                     else:
                         nr -= 1
                         nc += 1
-            elif nr < 0:
-                nr = 1
-                patternDown = False
-        
+                    
+    
         index = 0
         res = ""
         
