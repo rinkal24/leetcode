91200f7f7970830391c76086efc0d84aad69453e
Rinkal Shah <rnkl.shah@gmail.com>
2024-03-07 01:27:38 -0500

    Time: 46 ms (20.25%), Space: 16.6 MB (42.43%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -7,17 +7,19 @@ class Solution:
             ind = 0
             l1 = words[i][ind]
             l2 = words[i + 1][ind]
+            
             if l1 == l2:
                 while ind < len(words[i]) and ind < len(words[i + 1]):
                     l1 = words[i][ind] 
                     l2 = words[i + 1][ind] 
-                    #print(l1, l2)
                     if (l1 and l2 not in order) or (order.index(l1) > order.index(l2)):
-                        #print('here', l1, l2)
                         return False
                     ind += 1
+                    
                 if len(words[i]) > len(words[i + 1]) and l1 == l2:
                     return False
+                
             elif order.index(l1) > order.index(l2):
                 return False
+            
         return True
