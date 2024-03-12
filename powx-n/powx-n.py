c90eea00cab2dbf32b3f03d5f42ece7fe8bb1731
Rinkal Shah <rnkl.shah@gmail.com>
2024-03-12 03:34:11 -0400

    Time: 39 ms, Memory: 16.5 MB - LeetHub

---
+++ powx-n/powx-n.py
@@ -0,0 +1,15 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def helper(x, n):
+            if n == 0:
+                return 1
+            
+            if n < 0:
+                return 1/helper(x, -n)
+            
+            if n % 2 == 0:
+                return helper(x * x, n//2)
+            else:
+                return x * helper(x * x, (n - 1)//2)
+            return x * helper(x, n- 1)
+        return helper(x, n)
