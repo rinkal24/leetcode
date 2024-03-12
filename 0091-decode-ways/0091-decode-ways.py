57c3001b19fb09d2bc50b804f26dae38fd37c2db
Rinkal Shah <rnkl.shah@gmail.com>
2024-03-12 03:52:30 -0400

    Time: 39 ms (39.00%), Space: 16.5 MB (93.89%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,18 +1,15 @@
 class Solution:
-    @lru_cache(maxsize=None)
-    def helper(self, index, s):
-        if index == len(s):
-            return 1
-        if s[index] == '0':
-            return 0
-            
-        if index == len(s) - 1:
-            return 1
-            
-        ans = self.helper(index + 1, s)
-        if int(s[index: index + 2]) <= 26:
-            ans += self.helper(index + 2, s)
-        return ans
+    
     def numDecodings(self, s: str) -> int:
+        dp = [0 for _ in range(len(s) + 1)]
+        dp[0] = 1
+        dp[1] = 0 if s[0] == '0' else 1
         
-        return self.helper(0, s)
+        for i in range(2, len(dp)):
+            if s[i - 1]!= '0':
+                dp[i] = dp[i - 1]
+            
+            two_digit = int(s[i - 2:i])
+            if two_digit >= 10 and two_digit <= 26:
+                dp[i] += dp[i - 2]
+        return dp[len(s)]
