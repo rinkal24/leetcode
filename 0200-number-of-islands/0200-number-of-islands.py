c897d88a688f41d1f3e0e3afcbe9c4c473e4503d
Rinkal Shah <rnkl.shah@gmail.com>
2024-03-14 17:45:38 -0400

    Time: 244 ms (60.32%), Space: 18.9 MB (84.54%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,38 +1,29 @@
 class Solution:
+    def dfs(self, grid, r, c):
+        nr = len(grid)
+        nc = len(grid[0])
+        
+        if r < 0 or c < 0 or r >= nr or c >= nc or grid[r][c] == '0':
+            return 
+        grid[r][c] = '0'
+        self.dfs(grid, r - 1, c)
+        self.dfs(grid, r + 1, c)
+        self.dfs(grid, r, c - 1)
+        self.dfs(grid, r, c + 1)
         
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid or len(grid) == 0:
             return 0
         
+        num_Islands = 0
         nr = len(grid)
         nc = len(grid[0])
-        num_islands = 0
         
         for r in range(nr):
             for c in range(nc):
                 if grid[r][c] == '1':
+                    self.dfs(grid, r, c)
+                    num_Islands += 1
                     
-                    num_islands += 1
-                    grid[r][c] = '0'
-                    Q = collections.deque([])
-                    Q.append(r * nc + c)
-                    
-                    while Q:
-                        ex = Q.popleft()
-                        row = int(ex / nc)
-                        col = int(ex % nc)
-                        
-                        if row - 1 >= 0 and grid[row - 1][col] == '1':
-                            Q.append((row - 1) * nc + col)
-                            grid[row - 1][col] = '0'
-                        if row + 1 < nr and grid[row + 1][col] == '1':
-                            Q.append((row + 1) * nc + col)
-                            grid[row + 1][col] = '0'
-                        if col - 1 >= 0 and grid[row][col - 1] == '1':
-                            Q.append((row) * nc + col - 1)
-                            grid[row][col - 1] = '0'
-                        if col + 1 < nc and grid[row][col + 1] == '1':
-                            Q.append((row) * nc + col + 1)
-                            grid[row][col + 1] = '0'
-                    
-        return num_islands
+        return num_Islands
+        
