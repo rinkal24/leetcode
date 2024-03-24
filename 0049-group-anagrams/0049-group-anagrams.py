d92904ba4d09a9a23ae5c14d6479a6d301ad19b9
Rinkal Shah <rnkl.shah@gmail.com>
2024-03-23 20:01:57 -0400

    Time: 1175 ms (5.03%), Space: 20.8 MB (31.41%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,24 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = []
+        
+        words_sort = {}
+        ctr = 0
+        res = []
+        
+        for ind in range(len(strs)):
+            val = ''.join(sorted(strs[ind]))
+            if val in words_sort.keys():
+                words_sort[val].append(ind)
+            else:
+                words_sort[val] = [ind]
+                
+        res = [[] for _ in range(len(words_sort))]
+        
+        for words in strs:
+            val = ''.join(sorted(words))
+            ind_val = list(words_sort.keys()).index(val)
+            res[ind_val].append(words)
+            
+        
+        return res
