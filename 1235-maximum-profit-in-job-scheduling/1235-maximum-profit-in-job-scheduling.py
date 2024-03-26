87bafb6e272fcf8b7a505820b687324fca38e293
Rinkal Shah <rnkl.shah@gmail.com>
2024-03-26 17:22:39 -0400

    Time: 638 ms (70.59%), Space: 32.3 MB (54.87%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,19 +1,19 @@
-from heapq import heappush, heappop
-
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-      
-        jobs = list(zip(startTime, endTime, profit))
-        jobs.sort()
-        maxProfit = 0
         
+        jobs = list(zip(startTime, endTime, profit))
+        
+        jobs = sorted(jobs)
         heap = []
+        maxProfit = 0
         
         for start, end, profit in jobs:
             while heap and heap[0][0] <= start:
                 maxProfit = max(maxProfit, heappop(heap)[1])
-            heappush(heap, (end, profit + maxProfit ))
+            heappush(heap, (end, profit + maxProfit))
+            
         while heap:
             maxProfit = max(maxProfit, heappop(heap)[1])
+            
         
         return maxProfit
