0f6f466d25316eda8d5e842f1fdf04b796b17d76
Rinkal Shah <rnkl.shah@gmail.com>
2024-03-29 19:54:08 -0400

    Time: 93 ms (85.26%), Space: 17.9 MB (27.64%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,11 +1,21 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        limit = int(n/3)
         
-        ctr_n = Counter(nums)
+        limit = len(nums) // 3
+        ctr = 1
         res = []
-        for i in ctr_n:
-            if ctr_n[i] > limit:
-                res.append(i)
+        nums.sort()
+        
+        
+        for i in range(len(nums)):
+            if i > 0:
+                if nums[i] == nums[i - 1] and nums[i] not in res:
+                    ctr += 1
+                elif nums[i] != nums[i - 1]:
+                    ctr = 1
+            if ctr > limit and nums[i] not in res:
+                res.append(nums[i])
+                
+        if not res and len(nums) == 1: 
+            return nums
         return res
