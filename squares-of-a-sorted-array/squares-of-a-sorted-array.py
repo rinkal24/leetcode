96d0c7f2a4d6394158331ce2cdf1716acccdf59b
Rinkal Shah <rnkl.shah@gmail.com>
2024-04-03 16:26:53 -0400

    Time: 170 ms, Memory: 18.7 MB - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,8 +1,38 @@
 class Solution:
+    def reverseNums(self, neg):
+        nums_len = len(neg) - 1
+        ctr = 0
+        print(nums_len)
+        while ctr <= nums_len:
+            neg[ctr], neg[nums_len] = neg[nums_len], neg[ctr]
+            nums_len -= 1
+            ctr += 1
+        return neg
+    
     def sortedSquares(self, nums: List[int]) -> List[int]:
+        neg = []
         res = []
+        pos = 0
+        
         for i in nums:
-            res.append(i * i)
+            if i < 0:
+                neg.append(i)
+                pos += 1
+            else:
+                break
+               
             
-        return sorted(res)
+        neg = self.reverseNums(neg)
+        ctr = 0  
+        
+        while pos < len(nums) or ctr < len(neg):
+            
+            if (ctr < len(neg) and pos >= len(nums)) or (ctr < len(neg) and pos < len(nums) and abs(neg[ctr]) < nums[pos]):
+                    res.append(neg[ctr] * neg[ctr])
+                    ctr += 1
+            elif pos < len(nums):
+                res.append(nums[pos] * nums[pos])
+                pos += 1
+            
+        return res
             
