bdad481f0ab485752e3bf9c63e480592f1aba348
Rinkal Shah <rnkl.shah@gmail.com>
2024-04-15 16:45:10 -0400

    Time: 361 ms, Memory: 16.6 MB - LeetHub

--- longest-common-subsequence/longest-common-subsequence.py
+++ longest-common-subsequence/longest-common-subsequence.py
@@ -1,15 +1,21 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         
-        dp_grid = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
+        if len(text2) < len(text1):
+            text1, text2 = text2, text1
+            
+        prev = [0] * (len(text1) + 1)
+        curr = [0] * (len(text1) + 1)
         
         for col in reversed(range(len(text2))):
             for row in reversed(range(len(text1))):
                 
                 if text2[col] == text1[row]:
-                    dp_grid[row][col] = 1 + dp_grid[row + 1][col + 1]
+                    curr[row] = 1 + prev[row + 1]
                     
                 else:
-                    dp_grid[row][col] = max(dp_grid[row + 1][col], dp_grid[row][col + 1])
+                    curr[row] = max(prev[row], curr[row + 1])
                     
-        return dp_grid[0][0]
+            prev, curr = curr, prev
+                    
+        return prev[0]
