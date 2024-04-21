2626aea803ec94b20a12f36fd2ed36dbbc960233
Rinkal Shah <rnkl.shah@gmail.com>
2024-04-21 17:53:50 -0400

    Time: 39 ms (88.53%), Space: 17.9 MB (52.39%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -3,6 +3,7 @@ class Solution:
         ctr_s = Counter(s)
         
         ctr_s = sorted(ctr_s.items(), key = lambda x:x[1], reverse = True)
+        
         res = ""
         
         for i in range(len(ctr_s)):
